cd1d20810be3456833ee544799cad1f84287db8d
giasong123 <fnvl8323@gmail.com>
2023-10-24 16:01:26 +0900

    feat: header 영역 기초 완료

--- src/components/Footer.js
+++
@@ -1,4 +0,0 @@
-export default function Footer() {
-  //  javaScript코드자리
-  return <footer class="footer">하단</footer>;
-}
--- src/components/Header.js
+++
@@ -1,4 +0,0 @@
-export default function Header() {
-  //  javaScript코드자리
-  return <header class="header">상단</header>;
-}
