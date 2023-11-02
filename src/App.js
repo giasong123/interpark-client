bfa3392ba2d02ffb98256376ef88c8bdde5e35ba
giasong123 <fnvl8323@gmail.com>
2023-11-02 14:07:06 +0900

    feat: 중간과정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/jsx-no-undef */
 import Header from "./components/Header";
+import Visual from "./components/Visual";
+import Recommend from "./components/Recommend";
 
 function App() {
   return (
@@ -6,7 +9,10 @@ function App() {
       {/* <!-- 상단 영역 --> */}
       <Header />
       {/* <!-- 메인 영역 --> */}
-      <div className="main"></div>
+      <div className="main">
+        <Visual />
+        <Recommend />
+      </div>
       {/* <!-- 하단 영역 --> */}
       <footer className="footer"></footer>
     </div>
